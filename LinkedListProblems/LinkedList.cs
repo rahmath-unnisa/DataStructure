ef2e5ae5c1ee5ec6007286a5159ac892a6dea6a6
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-30 08:42:35 +0530

    [Rahmath]Add:Folder For Linked List Problems

---
+++ LinkedListProblems/LinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblems
+{
+    public class LinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted into Linked List", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkList Is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+    
+
+       
---
+++ LinkedListProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LinkedListProblems;
+
+namespace LinkedListProblems
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
